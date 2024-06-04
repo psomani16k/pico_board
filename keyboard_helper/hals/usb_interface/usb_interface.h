f42fe5cbe5f17df4ec730c05eb543c34f54b5a17
parth <psomani16k@gmail.com>
2024-06-04 15:17:39 +0530

    moved all usb code to usb_interface, a test passes

--- keyboard_helper/hals/usb_interface/usb_interface.h
+++ keyboard_helper/hals/usb_interface/usb_interface.h
@@ -47,6 +47,36 @@ void tud_hid_set_protocol_cb(uint8_t instance, uint8_t protocol)
     // TODO set a indicator for user
 }
 
+//--------------------------------------------------------------------+
+// Device callbacks
+//--------------------------------------------------------------------+
+
+// Invoked when device is mounted
+void tud_mount_cb(void)
+{
+    blink_interval_ms = BLINK_MOUNTED;
+}
+
+// Invoked when device is unmounted
+void tud_umount_cb(void)
+{
+    blink_interval_ms = BLINK_NOT_MOUNTED;
+}
+
+// Invoked when usb bus is suspended
+// remote_wakeup_en : if host allow us  to perform remote wakeup
+// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+void tud_suspend_cb(bool remote_wakeup_en)
+{
+    (void)remote_wakeup_en;
+    blink_interval_ms = BLINK_SUSPENDED;
+}
+
+// Invoked when usb bus is resumed
+void tud_resume_cb(void)
+{
+    blink_interval_ms = BLINK_MOUNTED;
+}
 
 // Invoked when received SET_REPORT control request or
 // received data on OUT endpoint ( Report ID = 0, Type = 0 )
@@ -76,7 +106,7 @@ void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_
             {
                 // Caplocks Off: back to normal blink
                 cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-                //blink_interval_ms = BLINK_MOUNTED;
+                // blink_interval_ms = BLINK_MOUNTED;
             }
         }
     }
@@ -109,3 +139,62 @@ void tud_hid_report_complete_cb(uint8_t instance, uint8_t const *report, uint16_
     // nothing to do
 }
 
+//--------------------------------------------------------------------+
+// USB HID
+//--------------------------------------------------------------------+
+
+// Every 10ms, we will sent 1 report for each HID profile (keyboard, mouse etc ..)
+// tud_hid_report_complete_cb() is used to send the next report after previous one is complete
+void hid_task(void)
+{
+    // Poll every 10ms
+    const uint32_t interval_ms = 500;
+    static uint32_t start_ms = 0;
+
+    if (to_ms_since_boot(get_absolute_time()) - start_ms < interval_ms)
+        return; // not enough time
+    start_ms += interval_ms;
+
+    uint32_t const btn = true;
+
+    if (tud_suspended() && btn)
+    {
+        // Wake up host if we are in suspend mode
+        // and REMOTE_WAKEUP feature is enabled by host
+        tud_remote_wakeup();
+    }
+    else
+    {
+        // keyboard interface
+        if (tud_hid_n_ready(ITF_NUM_KEYBOARD))
+        {
+            // used to avoid send multiple consecutive zero report for keyboard
+            static bool has_keyboard_key = false;
+
+            uint8_t const report_id = 0;
+            uint8_t const modifier = 0;
+
+            if (btn)
+            {
+                uint8_t keycode[6] = {0};
+                keycode[0] = HID_KEY_A;
+
+                tud_hid_n_keyboard_report(ITF_NUM_KEYBOARD, report_id, modifier, keycode);
+                has_keyboard_key = true;
+            }
+            else
+            {
+                // send empty key report if previously has key pressed
+                if (has_keyboard_key)
+                    tud_hid_n_keyboard_report(ITF_NUM_KEYBOARD, report_id, modifier, NULL);
+                has_keyboard_key = false;
+            }
+        }
+
+        // mouse interface
+        if (tud_hid_n_ready(ITF_NUM_MOUSE))
+        {
+            return;
+        }
+    }
+}
