cea4bb377739c7d1cc0e6bbc8aa576a41350e604
parth <psomani16k@gmail.com>
2024-06-04 15:14:58 +0530

    a test works, moved some functions

--- keyboard_helper/hals/usb_interface/usb_interface.h
+++ keyboard_helper/hals/usb_interface/usb_interface.h
@@ -46,3 +46,66 @@ void tud_hid_set_protocol_cb(uint8_t instance, uint8_t protocol)
     // nothing to do since we use the same compatible boot report for both Boot and Report mode.
     // TODO set a indicator for user
 }
+
+
+// Invoked when received SET_REPORT control request or
+// received data on OUT endpoint ( Report ID = 0, Type = 0 )
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize)
+{
+    (void)report_id;
+
+    // keyboard interface
+    if (instance == ITF_NUM_KEYBOARD)
+    {
+        // Set keyboard LED e.g Capslock, Numlock etc...
+        if (report_type == HID_REPORT_TYPE_OUTPUT)
+        {
+            // bufsize should be (at least) 1
+            if (bufsize < 1)
+                return;
+
+            uint8_t const kbd_leds = buffer[0];
+
+            if (kbd_leds & KEYBOARD_LED_CAPSLOCK)
+            {
+                // Capslock On: disable blink, turn led on
+                blink_interval_ms = 0;
+                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+            }
+            else
+            {
+                // Caplocks Off: back to normal blink
+                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+                //blink_interval_ms = BLINK_MOUNTED;
+            }
+        }
+    }
+}
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen)
+{
+    // TODO not Implemented
+    (void)instance;
+    (void)report_id;
+    (void)report_type;
+    (void)buffer;
+    (void)reqlen;
+
+    return 0;
+}
+
+// Invoked when sent REPORT successfully to host
+// Application can use this to send the next report
+// Note: For composite reports, report[0] is report ID
+void tud_hid_report_complete_cb(uint8_t instance, uint8_t const *report, uint16_t len)
+{
+    (void)instance;
+    (void)report;
+    (void)len;
+
+    // nothing to do
+}
+
